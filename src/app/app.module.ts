165fc4cea61079b1ff8e5813d3db0e74030e11db
witower <25256638+witower@users.noreply.github.com>
2020-06-19 03:46:34 +0200

    added ng-bootstrap and httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ShowTimeComponent } from './component/show-time/show-time.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ShowTimeComponent } from './component/show-time/show-time.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
